e30283ae95e2c27da84756d06c1a4812d54a2f5b
tnaveen-kumar <123799811+tnaveen-kumar@users.noreply.github.com>
2024-08-22 00:41:05 +0530

    Create variables.tf

---
+++ 03Variables-02/variables.tf
@@ -0,0 +1,22 @@
+variable "RG_Name_1" {
+  description = "Enter the resource group name"
+  default = "AzBang-03"
+}
+
+variable "RG_Name_2" {
+  description = "Enter the resource group name"
+  default = "AzBang-04"
+  
+}
+
+variable "RG_Location_1" {
+  description = "Enter the resource group location"
+  default = "South India"
+  
+}
+
+variable "RG_Location_2" {
+  description = "Enter the resource group location"
+  default = "Central India"
+  
+}
