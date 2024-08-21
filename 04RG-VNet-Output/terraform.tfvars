e9510f3cbb30fc64a292ede4a31155ba09ad5cf0
tnaveen-kumar <123799811+tnaveen-kumar@users.noreply.github.com>
2024-08-22 01:50:10 +0530

    Adding the new set of files

---
+++ 04RG-VNet-Output/terraform.tfvars
@@ -0,0 +1,3 @@
+RG_Name_1 = "AzureSQLBangalore-07"
+RG_Location_1 = "South India"
+VNet_Name = "AzBang-VNet"
---
+++ 04RG-VNet-Output/variables.tf
@@ -0,0 +1,14 @@
+variable "RG_Name_1" {
+  description = "Enter the resource group name"
+  default = "AzBang-03"
+}
+
+variable "RG_Location_1" {
+  description = "Enter the resource group location"
+  default = "South India"
+  
+}
+
+variable "VNet_Name" {
+  type = string
+}
