8689898587dd624b429cbe480fcf79b300efab76
tnaveen-kumar <123799811+tnaveen-kumar@users.noreply.github.com>
2024-08-22 23:05:52 +0530

    Create main.tf

---
+++ 07Looping/01Count/main.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+    features {
+    }
+  
+}
+
+variable "rg_names" {
+    type = list(string)
+    default = ["RG01", "RG02"]
+}
+
+variable "rg_location" {
+    default = "South India"
+  
+}
+
+resource "azurerm_resource_group" "r_rg_mame" {
+  count = length(var.rg_names)
+  name = "${var.rg_names[count.index]}-resourcegroup"
+  #name= var.rg_names[count.index]}
+  location = var.rg_location
+}
